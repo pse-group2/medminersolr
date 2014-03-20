c6f92615cd44223d2e13d5332cd3b5a93338dc48
Neyah Rizzello <n.rizzello@hotmail.com>
2014-03-20 17:41:06 +0100

    suche funktioniert jetzt

--- app/controllers/text_mining_controller.rb
+++ app/controllers/text_mining_controller.rb
@@ -1,12 +1,21 @@
 class TextMiningController < ApplicationController
   def search
    @text = params[:search_text]
-   
-   t = Text.search do 
-   	fulltext @text
-   end
+   @pages = Array.new
 
-   @result = t.results
+   search = Sunspot.search(Text) do |query|
+	query.fulltext @text
+   end
+   result = search.results
+   
+	if result.present? 
+	result.each do |result|
+		id = result.old_id
+		id_page = Revision.where(:rev_text_id => id).first.rev_page
+		@pages.push(Page.where(:page_id => id_page).first)
+		end
+	end
+	
 
   end
 
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,9 @@
 class Page < ActiveRecord::Base
+self.table_name = "page"
+
+searchable do
+  	text :page_title
+  	integer :page_id
+  	
+  end
 end
