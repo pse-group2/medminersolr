afbab8ba96d18a4e2bb6d5f3deba7adb6d2a2086
awaelchli <adrian.waelchli@students.unibe.ch>
2014-05-02 00:36:33 +0200

    refactoring

--- app/controllers/text_mining_controller.rb
+++ app/controllers/text_mining_controller.rb
@@ -1,23 +1,30 @@
 class TextMiningController < ApplicationController
+  
   def search
     @text = params[:search_text]
-    
+
     unless @text.blank? then
       engine = SearchEngine.new(@text)
       list = engine.search_results
-      
-      @results = Array.new
-      unless list.blank? then
+      @results = get_pages_from_hits(list)
+      @used_keywords = engine.used_keywords
+    end
+    
+  end
+
+  private
+
+  def get_pages_from_hits(list)
+    results = Array.new
+
+    unless list.blank? then
       list.all.each do |hit|
         key = hit.primary_key
         page = Page.where(:text_id => key).first
-        @results.push page
-      end
+        results.push page
       end
     end
-
+    results
   end
 
-  def results
-  end
 end
