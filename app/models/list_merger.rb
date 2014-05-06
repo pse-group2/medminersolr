ca724eb4206ccc80e4e2f351f45f25aeaa84e4fb
awaelchli <adrian.waelchli@students.unibe.ch>
2014-05-06 14:12:35 +0200

    fixed the keuchhusten bug.

--- app/models/list_merger.rb
+++ app/models/list_merger.rb
@@ -1,11 +1,9 @@
 # This is a helper class for merging multiple lists of search results.
 class ListMerger
-
-  
   def initialize
     @lists = Array.new
   end
-  
+
   def push (results_list)
     @lists.push results_list
   end
@@ -13,44 +11,48 @@ class ListMerger
   def clear
     @lists.clear
   end
-  
+
   #returns nil if there are no lists to merge
   def merge
 
     temp_intersection = ResultsList.new([],[])
-    
+
     if !@lists.empty?
       temp_intersection = @lists.first
-    
+
       @lists.each do |list|
         common_articles = temp_intersection.intersect list
         puts common_articles.count
         if common_articles.count > 0
-          temp_intersection = reorder(temp_intersection, list, common_articles) 
+          temp_intersection = reorder(temp_intersection, list, common_articles)
         end
       end
     end
-    
+
     temp_intersection
   end
-  
-  private 
-  
+
+  private
+
   def reorder(list1, list2, intersection)
-    
+
     reordered = Array.new(intersection.count  )
-    
+
     intersection.all.each do |hit|
       index1 = list1.all.index {|h| h.primary_key == hit.primary_key}
       index2 = list2.all.index {|h| h.primary_key == hit.primary_key}
-      
+
       new_index = (index1 + index2) / 2
       new_index = new_index.round
       
-      reordered[new_index] = hit
+      puts "old 1: #{index1} old 2: #{index2} new: #{new_index}"
+      
+      reordered.insert(new_index, hit)
+      # reordered[new_index] = hit
     end
-     puts intersection.used_keywords.to_s
-     ResultsList.new(reordered, intersection.used_keywords)
+    puts '_________'
+    puts reordered
+    ResultsList.new(reordered, intersection.used_keywords)
   end
-  
+
 end
