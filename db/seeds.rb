45d6234f89796a35187f9e9edffb897728da4286
Neyah Rizzello <n.rizzello@hotmail.com>
2014-05-07 13:15:41 +0200

    synonyms filling algorithm, not working yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@
 
 
 #Fill in the terms from external json
+
 json = File.read('icd_2012_ch_de.json')
 terms = JSON.parse(json)
 
@@ -18,18 +19,55 @@ terms.each do |term|
   
   synonyms = term['synonyms']
   
-  delim = ','
-  line = %Q(") << text << %Q(") << delim
   
-  synonyms.each do |synonym|
-    line = line << %Q(") << synonym << %Q(") << delim
-  end
+  delim1 = ' => '
+  delim2 = ','
   
-  line = line.chomp(delim)
+  puts text
+  unless TextProcessor.new(text).word_count != 1  then
+    
+    synonyms = synonyms.map { |syn| syn = TextProcessor.new(syn).word_count == 1 ? syn : nil }
+    synonyms = synonyms.compact 
+    
+    line = text << delim1
+    synonyms.each do |synonym|
+      line = line  << synonym << delim2
+    end
+    line = line.chomp(delim2)
   
-  unless synonyms.empty? then
-    file.puts line
-  end
+    unless synonyms.empty? then
+      file.puts line
+    end
+    
+  end  
+  
+  
+  # synonyms.each do |synonym|
+    # line = synonym << delim1
+    # line = line << text << delim2
+    # synonyms.each do |synonym1|
+         # line = line << synonym1 << delim2
+    # end
+    # #unless synonyms.empty? then
+    # #  file.puts line
+    # #end
+  # end
+  # line = line.chomp(delim2)
+# 
+
+
+  #delim = ','
+  #line = %Q(") << text << %Q(") << delim
+  
+  #synonyms.each do |synonym|
+  #  line = line << %Q(") << synonym << %Q(") << delim
+  #end
+  
+  #line = line.chomp(delim)
+#   
+  # unless synonyms.empty? then
+    # file.puts line
+  # end
 end
 
 file.close
