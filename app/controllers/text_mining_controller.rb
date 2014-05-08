a1b4b008e108f347223bf067c254969d844e9aa6
awaelchli <adrian.waelchli@students.unibe.ch>
2014-05-08 10:08:02 +0200

    better keywords display

--- app/controllers/text_mining_controller.rb
+++ app/controllers/text_mining_controller.rb
@@ -6,8 +6,20 @@ class TextMiningController < ApplicationController
     unless @text.blank? then
       engine = SearchEngine.new(@text)
       @results = engine.search_results
-      @used_keywords = engine.used_keywords
+      @used_keywords = printable_used_keywords(engine.used_keywords)
     end
     
   end
+  
+  private
+  
+  def printable_used_keywords(keywords_array)
+    comma = ', '
+    string = ''
+    keywords_array.each do |word|
+      string << word << ', '
+    end
+    string = string.chomp(comma)
+  end
+   
 end
