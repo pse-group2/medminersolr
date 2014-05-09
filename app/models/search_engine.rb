3b5f63b11d34b1fbae4e2db17d89a0096d9e00c7
awaelchli <adrian.waelchli@students.unibe.ch>
2014-05-10 01:02:10 +0200

    code documentation and readability improved

--- app/models/search_engine.rb
+++ app/models/search_engine.rb
@@ -1,60 +1,83 @@
 class SearchEngine
-
-  attr_reader :input, :used_keywords
+  
+  # The maximum of results requested from Solr.
+  MAX_RESULTS = 500
+  # The amount of boost applied to the score if a part of
+  # the input text occurs in the title of an article.
+  PAGE_TITLE_BOOST = 0.5
+  
+  DIMENSION_WORDS_FILE_PATH = Rails.root.to_s + "/solr/conf/dimensionwords.txt"
+  
+  attr_reader :input, :used_keywords, :dimensionwords
+  
   def initialize(text)
     @input = text
     @processor = TextProcessor.new(text)
-    @result_lists = ListMerger.new
+    @results_lists = ListMerger.new
     @used_keywords = Array.new
     @dimensionwords = Array.new
   end
 
+  # Returns a sorted ResultsList containing the most relevant articles for 
+  # the given input.
   def search_results
-    @result_lists.clear
+    @results_lists.clear
 
     search_for_keywords
+    single_list = @results_lists.merge
     
-    single_list = @result_lists.merge
     @used_keywords = single_list.used_keywords
     mark_hits_containing_dimensionwords(single_list)
+    
     single_list
   end
 
   private
 
+  # Applies the search only for words that are not dimension words.
   def search_for_keywords
-    reduced_words = reduce_keywords
-    keyword_search(reduced_words)
+    keywords = filter_dimensionwords(@processor.words)
+    keyword_search(keywords)
   end
   
-  def reduce_keywords
-    f = File.open('./solr/conf/dimensionwords.txt')
-    all_dimensionwords = Array.new
-    all_dimensionwords = f.read.split("\n")
-    reduced_words = @processor.words.map(&:downcase) - all_dimensionwords.compact.map(&:downcase)
-    @dimensionwords = @processor.words.map(&:downcase) - reduced_words
-    reduced_words
+  # Filters the given words_array for dimension words. The filtered out
+  # dimension words get stored. Returns an array containing no dimension
+  # words.
+  def filter_dimensionwords(words_array)
+    file = File.open(DIMENSION_WORDS_FILE_PATH)
+    all_dimensionwords = file.read.split("\n")
+    all_dimensionwords = all_dimensionwords.compact.map(&:downcase)
     
+    words = words_array.map(&:downcase)
+    
+    keywords =  words - all_dimensionwords
+    @dimensionwords = words - keywords
+    
+    keywords
   end
   
+  # Produces a search request for every word in the keywords_array.
   def keyword_search(keywords_array)
     keywords_array.each do |keyword|
       fulltext_search(keyword)
     end
   end
 
+  # The full-text search is applied to the content of the table Text and 
+  # the result get pushed onto the list of results_lists to later merge them
+  # together.
   def fulltext_search(text)
       search = Text.search do
         fulltext text do
-          fields(:content,:page => 0.5)
+          fields(:content,:page => PAGE_TITLE_BOOST)
         end
-        paginate :page => 1, :per_page => 1000
+        paginate :page => 1, :per_page => MAX_RESULTS
       end
       
     search_hits = search.hits.map {|hit| SearchHit.new(hit)}
     
     results = ResultsList.new(search_hits, [text])
-    @result_lists.push results
+    @results_lists.push results
   end
   
   def mark_hits_containing_dimensionwords(results_list)
