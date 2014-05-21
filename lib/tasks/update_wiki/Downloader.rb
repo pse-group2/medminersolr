ba612b5a898dfef8634aad25aa69afcfc89d8dc6
Florian Matter <folranpls@gmail.com>
2014-05-21 14:24:02 +0200

    fixed confused files

--- lib/tasks/update_wiki/Downloader.rb
+++ lib/tasks/update_wiki/Downloader.rb
@@ -29,21 +29,22 @@ class Downloader
   
   def startDownload
     @input.each do |name|
-      
       while @client.query("SELECT * FROM page WHERE page_id = #{name["page_id"]}").first.nil? do
         
         begin 
           
-          url = "http://de.wikipedia.org/w/index.php?curid=#{name["page_id"]}"
-          doc = Nokogiri::HTML(openURL(url))
-          text = ''
-          
-          doc.css('p,h1').each do |e|
-            text << e.content
+          page_id = name["page_id"].to_i
+          page_title = name["page_title"]
+          text_id = name["text_id"].to_i
+          if @client.query("SELECT * FROM page WHERE page_id = #{page_id}").count == 1
+            if @client.query("SELECT text_id FROM page WHERE page_id = #{page_id}").each[0]["text_id"] != text_id
+              @client.query("DELETE FROM page WHERE page_id=#{page_id}")
+              @client.query("DELETE FROM text WHERE page_id=#{page_id}")
+              downloadArticle(page_id, text_id, page_title)
+            end
+          else
+            downloadArticle(page_id, text_id, page_title)
           end
-          
-          @client.query("INSERT INTO page (page_id, page_title, text_id) VALUES(#{name["page_id"]}, '#{name["page_title"].gsub("'", %q(\\\'))}', #{name["text_id"]})")
-          @client.query("INSERT INTO text (page_id, content, text_id) VALUES(#{name["page_id"]}, '#{text.gsub("'", %q(\\\'))}', #{name["text_id"]})")
         
         rescue SocketError
           puts "\nWikipedia blocked the request, retrying in 10 seconds."
@@ -53,6 +54,17 @@ class Downloader
       end
       @c+=1
     end
+  end
+  
+  def downloadArticle(page_id, text_id, page_title)
+    url = "http://de.wikipedia.org/w/index.php?curid=#{page_id}"
+    doc = Nokogiri::HTML(openURL(url))
+    text = ''
+    doc.css('p,h1').each do |e|
+      text << e.content
+    end
+    @client.query("INSERT INTO page (page_id, page_title, text_id) VALUES(#{page_id}, '#{page_title.gsub("'", %q(\\\'))}', #{text_id})")
+    @client.query("INSERT INTO text (page_id, content, text_id) VALUES(#{page_id}, '#{text.gsub("'", %q(\\\'))}', #{text_id})")
   end
   
 end
